f6ed0178ab3770cb6aebd5a7da3c38f5d6b36b70
Hunter Davis <hunter@hunterdavis.com>
2016-10-30 15:52:12 -0700

    adding name generation

--- app/src/main/java/com/hunterdavis/adventureneverstops/utils/naming/NameUtils.java
+++ app/src/main/java/com/hunterdavis/adventureneverstops/utils/naming/NameUtils.java
@@ -10,6 +10,43 @@ public class NameUtils {
     }
 
     public static String generatePlayerName(String baseSeed) {
-        return baseSeed + "hunter";
+        return prefix() + " " +
+                generateFirstNaame(baseSeed) + " " +
+                generateLastNaame() + " " +
+                suffixTitleModifier() + " " +
+                metalAndLandType();
+    }
+
+    public static String generateFirstNaame(String baseSeed) {
+        return consonent() + vowel() + vowel() + baseSeed + consonent() + vowel() + consonent();
+    }
+
+    public static String generateLastNaame() {
+        return vowel() + consonent() + vowel() + consonent() + vowel() + vowel() + consonent();
+    }
+
+
+    public static String consonent() {
+        return "";
+    }
+
+    public static String vowel() {
+        return "";
+    }
+
+    public static String prefix() {
+        return "";
+    }
+
+    public static String suffixTitleModifier() {
+        return "";
+    }
+
+    public static String suffix() {
+        return "";
+    }
+
+    public static String metalAndLandType() {
+        return "" + "" + "";
     }
 }
