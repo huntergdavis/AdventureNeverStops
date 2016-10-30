bee81eac20a169ef7f75974ca1033be46522aa12
Hunter Davis <hunter@hunterdavis.com>
2016-10-30 11:32:58 -0700

    object model updates

--- app/src/main/java/com/hunterdavis/adventureneverstops/objects/game/GameState.java
+++ app/src/main/java/com/hunterdavis/adventureneverstops/objects/game/GameState.java
@@ -1,18 +1,25 @@
 package com.hunterdavis.adventureneverstops.objects.game;
 
+import java.util.Date;
+
 /**
  * Created by hunter on 10/30/16.
  */
 public class GameState {
 
     public Player currentPlayer;
+    public long epochCreated;
+    public long epochLastUpdated;
 
     public GameState() {
         currentPlayer = new Player();
+        epochCreated = epochLastUpdated = System.currentTimeMillis();
     }
 
     public GameState(GameState g2) {
-        this.currentPlayer = g2.currentPlayer;
+        this.currentPlayer = new Player(g2.currentPlayer);
+        this.epochCreated = g2.epochCreated;
+        this.epochLastUpdated = g2.epochLastUpdated;
     }
 
     public static GameState generateGameState() {
--- app/src/main/java/com/hunterdavis/adventureneverstops/objects/game/Player.java
+++ app/src/main/java/com/hunterdavis/adventureneverstops/objects/game/Player.java
@@ -2,15 +2,43 @@ package com.hunterdavis.adventureneverstops.objects.game;
 
 import com.hunterdavis.adventureneverstops.utils.naming.NameUtils;
 
+import java.util.ArrayList;
+
 public class Player {
-    public String name = "Player 1";
+    public String name = "";
+
+    // A player has a singluar 'hero', the main character in the journey
+    public Character hero = null;
+
+    // A player could have many squadrons following his hero
+    // a group of characters is known as an 'squadron'
+    // a player's group of squadroms is her 'army'
+    public int defaultArmySize = 4;
+    public int defaultNumberOfArmies = 2;
+    public ArrayList<Squadron> army = null;
+
 
     public Player() {
         this.name = NameUtils.generatePlayerName();
+        this.hero = Character.generateCharacter();
+
+        this.army = new ArrayList<>(defaultNumberOfArmies);
+        for(int i = 0; i < defaultNumberOfArmies; i++) {
+            this.army.add(Squadron.generateArmy(defaultArmySize));
+        }
+
+
     }
 
     public Player(Player p2) {
         this.name = p2.name;
+        this.hero = p2.hero;
+
+        this.army = new ArrayList<>(p2.army.size());
+        for(Squadron squad : p2.army) {
+            this.army.add(new Squadron(squad));
+        }
+
     }
 
 
