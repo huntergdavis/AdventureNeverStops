73355c1a768daa7d9c6a935806c922c9698d2509
Hunter Davis <hunter@hunterdavis.com>
2016-12-25 15:35:13 -0800

    updating with a blank map view and a custom view for backstory

---
+++ app/src/main/java/com/hunterdavis/adventureneverstops/views/PrologueView.java
@@ -0,0 +1,57 @@
+package com.hunterdavis.adventureneverstops.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hunterdavis.adventureneverstops.R;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class PrologueView extends View {
+
+    public PrologueView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public PrologueView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public PrologueView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.PrologueView, defStyle, 0);
+
+
+        a.recycle();
+
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // TODO: consider storing these as member variables to reduce
+        // allocations per draw cycle.
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+
+    }
+}
