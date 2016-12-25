4a06e005b8bed658b5160c8db5192d26a949f2ac
Hunter Davis <hunter@hunterdavis.com>
2016-12-25 15:35:27 -0800

    updating with a custom map view (blank) and a custom view for prologue, plus attrs and colors update

--- app/src/main/java/com/hunterdavis/adventureneverstops/fragments/GameViewFragment.java
+++ app/src/main/java/com/hunterdavis/adventureneverstops/fragments/GameViewFragment.java
@@ -17,6 +17,7 @@ import com.hunterdavis.adventureneverstops.R;
 import com.hunterdavis.adventureneverstops.activities.GameView;
 import com.hunterdavis.adventureneverstops.engine.AdventureEngine;
 import com.hunterdavis.adventureneverstops.events.HeroUpdatedEvent;
+import com.hunterdavis.adventureneverstops.views.PrologueView;
 import com.squareup.otto.Subscribe;
 
 import butterknife.BindView;
@@ -26,11 +27,8 @@ import butterknife.OnClick;
 public class GameViewFragment extends Fragment {
 
 
-    @BindView(R.id.history_text)
-    TextView historyText;
-
-    @BindView(R.id.current_exp)
-    TextView currentExp;
+    @BindView(R.id.prologue_view)
+    PrologueView prologueView;
 
     private int offsetNumber = 0;
 
@@ -70,24 +68,9 @@ public class GameViewFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_game_view, container, false);
         ButterKnife.bind(this, v);
 
-        historyText.setText(adventureEngine.state.currentPlayer.hero.backStory());
-
-        setCurrentHeroExp(adventureEngine.state.currentPlayer.hero.experience);
-
         return v;
     }
 
-    @Subscribe
-    public void updateHero(HeroUpdatedEvent heroUpdatedEvent) {
-        if(isAdded()) {
-            setCurrentHeroExp(heroUpdatedEvent.hero.experience);
-        }
-    }
-
-    @UiThread
-    private void setCurrentHeroExp(long exp) {
-        currentExp.setText(getString(R.string.current_experience, exp));
-    }
 
 
 
--- app/src/main/java/com/hunterdavis/adventureneverstops/views/PrologueView.java
+++ app/src/main/java/com/hunterdavis/adventureneverstops/views/PrologueView.java
@@ -3,15 +3,30 @@ package com.hunterdavis.adventureneverstops.views;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.support.annotation.UiThread;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.hunterdavis.adventureneverstops.ANSApplication;
 import com.hunterdavis.adventureneverstops.R;
+import com.hunterdavis.adventureneverstops.events.HeroUpdatedEvent;
+import com.squareup.otto.Subscribe;
 
-/**
- * TODO: document your custom view class.
- */
-public class PrologueView extends View {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class PrologueView extends LinearLayout {
+
+
+    @BindView(R.id.history_text)
+    TextView historyText;
+
+    @BindView(R.id.current_exp)
+    TextView currentExp;
 
     public PrologueView(Context context) {
         super(context);
@@ -36,6 +51,11 @@ public class PrologueView extends View {
 
         a.recycle();
 
+        View v = View.inflate(getContext(), R.layout.prologue_view, this);
+        ButterKnife.bind(v, this);
+
+        ANSApplication.getEventBus().register(this);
+
     }
 
 
@@ -54,4 +74,23 @@ public class PrologueView extends View {
         int contentHeight = getHeight() - paddingTop - paddingBottom;
 
     }
+
+
+
+    @Subscribe
+    public void updateHero(HeroUpdatedEvent heroUpdatedEvent) {
+        setCurrentHeroExp(heroUpdatedEvent);
+        updateHistoryText(heroUpdatedEvent);
+    }
+
+    private void updateHistoryText(HeroUpdatedEvent heroUpdatedEvent) {
+        historyText.setText(heroUpdatedEvent.hero.backStory());
+    }
+
+    @UiThread
+    private void setCurrentHeroExp(HeroUpdatedEvent heroUpdatedEvent) {
+        currentExp.setText(getContext().getString(R.string.current_experience, heroUpdatedEvent.hero.experience));
+    }
+
+
 }
